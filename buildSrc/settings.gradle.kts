87cdd49dbb27fea564e64e5f1a832ab4aa8480dd
diskria <diskria@proton.me>
2025-09-14 22:07:04 +0400

    ci: temporary use explicit settings script

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,6 +1,44 @@
-apply(from = "../../bisectrix/settings/project.settings.gradle.kts")
-apply(from = "../../bisectrix/settings/android-app.settings.gradle.kts")
-apply(from = "../../bisectrix/settings/minecraft-mod.settings.gradle.kts")
+fun RepositoryHandler.commonRepositories() {
+    mavenLocal()
+    mavenCentral()
+    gradlePluginPortal()
+    google()
+
+    maven("https://libraries.minecraft.net") {
+        name = "Minecraft Libraries"
+    }
+    maven("https://maven.fabricmc.net") {
+        name = "Fabric"
+    }
+    exclusiveContent {
+        forRepository {
+            maven("https://repo.spongepowered.org/repository/maven-public") {
+                name = "SpongePowered"
+            }
+        }
+        filter {
+            @Suppress("UnstableApiUsage")
+            includeGroupAndSubgroups("org.spongepowered")
+        }
+    }
+}
+
+fun setupRepositories() {
+    dependencyResolutionManagement {
+        @Suppress("UnstableApiUsage")
+        repositories {
+            commonRepositories()
+        }
+    }
+
+    pluginManagement {
+        repositories {
+            commonRepositories()
+        }
+    }
+}
+
+setupRepositories()
 
 dependencyResolutionManagement {
     versionCatalogs {
