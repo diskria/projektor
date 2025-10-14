1d559e1bdcd4369f83a708a583bb0c2e7be4b335
diskria <diskria@proton.me>
2025-10-14 21:10:42 +0400

    feat: release 3.2.10
    - idk

--- project-plugin/src/main/kotlin/io/github/diskria/projektor/publishing/maven/GithubPages.kt
+++ project-plugin/src/main/kotlin/io/github/diskria/projektor/publishing/maven/GithubPages.kt
@@ -2,12 +2,16 @@ package io.github.diskria.projektor.publishing.maven
 
 import io.github.diskria.gradle.utils.extensions.getBuildDirectory
 import io.github.diskria.gradle.utils.extensions.getDirectory
+import io.github.diskria.gradle.utils.extensions.hasTask
 import io.github.diskria.gradle.utils.extensions.registerTask
+import io.github.diskria.projektor.common.projekt.metadata.ProjektMetadata
 import io.github.diskria.projektor.projekt.common.IProjekt
 import io.github.diskria.projektor.readme.shields.common.ReadmeShield
 import io.github.diskria.projektor.readme.shields.dynamic.GithubPagesShield
 import io.github.diskria.projektor.tasks.release.ReleaseToGithubPagesTask
 import org.gradle.api.Project
+import org.gradle.internal.extensions.core.extra
+import kotlin.collections.getValue
 
 data object GithubPages : LocalMaven() {
 
@@ -15,10 +19,16 @@ data object GithubPages : LocalMaven() {
 
     override fun configure(projekt: IProjekt, project: Project) {
         super.configure(projekt, project)
-        project.registerTask<ReleaseToGithubPagesTask> {
-            dependsOn(getConfigurePublicationTaskName())
-            localMavenDirectory.set(project.rootProject.getBuildDirectory(DIRECTORY_NAME))
-            githubPagesMavenDirectory.set(project.rootProject.getDirectory(MAVEN_DIRECTORY_NAME))
+        val rootProject = project.rootProject
+        if (!rootProject.hasTask<ReleaseToGithubPagesTask>()) {
+            rootProject.registerTask<ReleaseToGithubPagesTask> {
+                dependsOn(getConfigurePublicationTaskName())
+
+                val projektMetadata: ProjektMetadata by rootProject.extra.properties
+                metadata.set(projektMetadata)
+                localMavenDirectory.set(rootProject.getBuildDirectory(DIRECTORY_NAME))
+                githubPagesMavenDirectory.set(rootProject.getDirectory(MAVEN_DIRECTORY_NAME))
+            }
         }
     }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ plugins {
 
 projekt {
     description = "Gradle plugin with reusable conventions and helpers for projects from my GitHub organizations."
-    version = "3.2.9"
+    version = "3.2.10"
     license = MIT
     tags = setOf("configuration")
 
