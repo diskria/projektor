52938043e821483b49a9a85ac2407ea52a77432f
diskria <diskria@proton.me>
2025-10-15 12:11:00 +0400

    chore: 3.3.4 release for github pages release task test on ci

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ plugins {
 
 projekt {
     description = "Gradle plugin with reusable conventions and helpers for projects from my GitHub organizations."
-    version = "3.3.3"
+    version = "3.3.4"
     license = MIT
     tags = setOf("configuration")
 
