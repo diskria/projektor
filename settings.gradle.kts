7bd950fc2911db45b0d94ca16f0e8c0d6f1391f4
gradle-plugin <diskria@proton.me>
2025-10-15 13:03:02 +0400

    feat: release 3.3.6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ plugins {
 
 projekt {
     description = "Gradle plugin with reusable conventions and helpers for projects from my GitHub organizations."
-    version = "3.3.5"
+    version = "3.3.6"
     license = MIT
     tags = setOf("configuration")
 
