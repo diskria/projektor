e6a07a96ac5d08caf8fcff0a51b26a220ac81535
gradle-plugin <diskria@proton.me>
2025-10-17 09:23:17 +0400

    fix: stabilize GitHub Pages release task

--- project-plugin/src/main/kotlin/io/github/diskria/projektor/tasks/release/ReleaseToGithubPagesTask.kt
+++ project-plugin/src/main/kotlin/io/github/diskria/projektor/tasks/release/ReleaseToGithubPagesTask.kt
@@ -7,7 +7,6 @@ import io.github.diskria.projektor.common.projekt.metadata.ProjektMetadata
 import io.github.diskria.projektor.publishing.maven.GithubPages
 import io.github.diskria.projektor.publishing.maven.common.LocalMaven.Companion.LOCAL_MAVEN_DIRECTORY_NAME
 import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Sync
 import org.gradle.internal.extensions.core.extra
@@ -15,28 +14,22 @@ import org.gradle.internal.extensions.core.extra
 abstract class ReleaseToGithubPagesTask : Sync() {
 
     @get:Internal
-    abstract val metadata: Property<ProjektMetadata>
-
-    @get:Internal
     abstract val repositoryDirectory: DirectoryProperty
 
     init {
-        val projektMetadata: ProjektMetadata by project.extra.properties
-
         dependsOn(GithubPages.getConfigurePublicationTaskName())
 
-        metadata.convention(projektMetadata)
         repositoryDirectory.convention(project.layout.projectDirectory)
 
         from(project.getBuildDirectory(LOCAL_MAVEN_DIRECTORY_NAME))
         into(project.getDirectory(GITHUB_PAGES_MAVEN_DIRECTORY_NAME))
 
         doLast {
-            val metadata = metadata.get()
+            val projektMetadata: ProjektMetadata by project.extra.properties
             val repositoryDirectory = repositoryDirectory.get().asFile
 
             with(GitShell.open(repositoryDirectory)) {
-                val owner = metadata.repository.owner
+                val owner = projektMetadata.repository.owner
                 configureUser(owner.name, owner.email)
                 stage(destinationDir.relativeTo(repositoryDirectory).path)
                 commit("chore: deploy ${projektMetadata.version} release to GitHub Pages")
