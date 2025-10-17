26fe5835e22508a46d6b9572f54bc34103f51d82
projektor <diskria@proton.me>
2025-10-17 21:08:29 +0400

    chore: release 3.4.17 to test release task on CI

--- settings-plugin/src/main/kotlin/io/github/diskria/projektor/settings/configurations/AndroidApplicationConfiguration.kt
+++ settings-plugin/src/main/kotlin/io/github/diskria/projektor/settings/configurations/AndroidApplicationConfiguration.kt
@@ -1,5 +1,3 @@
 package io.github.diskria.projektor.settings.configurations
 
-import io.github.diskria.projektor.common.configurations.IAndroidApplicationConfiguration
-
 open class AndroidApplicationConfiguration
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,4 @@
 import io.github.diskria.projektor.common.licenses.LicenseType.MIT
-import io.github.diskria.projektor.common.projekt.ProjektType.MINECRAFT_MOD
 import io.github.diskria.projektor.common.publishing.PublishingTargetType.GITHUB_PAGES
 import io.github.diskria.projektor.settings.configurators.MinecraftModConfigurator
 
@@ -22,7 +21,7 @@ plugins {
 }
 
 projekt {
-    version = "3.4.15"
+    version = "3.4.17"
     license = MIT
     publish = GITHUB_PAGES
 
