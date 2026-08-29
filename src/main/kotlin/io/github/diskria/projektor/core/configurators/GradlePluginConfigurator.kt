1e9ed0b36fc4855441f063e5e067ed270f67c53b
diskria <diskria@proton.me>
2026-08-29 12:41:21 +0400

    fix: change `gradleKotlinDsl()` dependency to compileOnly in plugin projects

--- src/main/kotlin/io/github/diskria/projektor/core/configurators/GradlePluginConfigurator.kt
+++ src/main/kotlin/io/github/diskria/projektor/core/configurators/GradlePluginConfigurator.kt
@@ -39,7 +39,7 @@ internal class GradlePluginConfigurator(
             }
         }
         project.dependencies {
-            "implementation"(project.gradleKotlinDsl())
+            "compileOnly"(project.gradleKotlinDsl())
         }
     }
 }
